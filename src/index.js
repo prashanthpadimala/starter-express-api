9eeb4ecc373713d2c541365e10ba3d1eae278461
prashanthpadimala <padimalaprashanth@gmail.com>
2023-11-02 12:58:55 +0530

    [Task] new test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo! PY')
+    res.send('Yo! PY index is running')
 })
 app.listen(process.env.PORT || 3004)
